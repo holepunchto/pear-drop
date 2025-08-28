8e9b1775ebb82a386e21b8ed61928b27b503bfe0
dmc <david.mark.clements@gmail.com>
2025-08-28 13:45:13 +0200

    links with keys that have aliases mapped to alias

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 'use strict'
 const ref = require('pear-ref')
+const pearLink = require('pear-link')
 module.exports = function drop (link, opts) {
+  link = pearLink.parse(link).origin
   const ipc = global.Pear?.[global.Pear?.constructor.IPC] ?? global.Pear?.[Symbol.for('pear.ipc')]
   if (!ipc) throw new Error('pear-drop is designed for Pear - IPC missing')
   ref.ref()
--- test/index.test.js
+++ test/index.test.js
@@ -3,6 +3,8 @@ const test = require('brittle')
 const b4a = require('b4a')
 const sodium = require('sodium-native')
 const IPC = require('pear-ipc')
+const { ALIASES } = require('pear-aliases')
+const { encode } = require('hypercore-id-encoding')
 const Iambus = require('iambus')
 const { isWindows } = require('which-runtime')
 const drop = require('..')
@@ -84,5 +86,39 @@ test('drop(link, opts) (v1)', async (t) => {
   const stream = drop(link, opts)
   stream.on('data', (msg) => {
     t.alike({ some: 'info', params: { link, ...opts } }, msg)
+  })
+})
+
+test('drop(linkWithKeyThatHasAlias, opts)', async (t) => {
+  t.plan(1)
+  const kIPC = Symbol('test.ipc')
+  const socketPath = isWindows ? `\\\\.\\pipe\\test-${pipeId(__dirname)}` : __dirname + '/test.sock' // eslint-disable-line
+  const bus = new Iambus()
+  const srv = new IPC.Server({
+    socketPath,
+    handlers: {
+      drop (params) {
+        const sub = bus.sub({ params })
+        bus.pub({ some: 'info', params })
+        setImmediate(() => sub.end())
+        return sub
+      }
+    }
+  })
+  t.teardown(() => srv.close())
+  await srv.ready()
+  const ipc = new IPC.Client({ socketPath })
+  t.teardown(() => ipc.close())
+  await ipc.ready()
+  class API {
+    static IPC = kIPC
+    get [kIPC] () { return ipc }
+  }
+  global.Pear = new API()
+  const opts = { dir: 'some/path' }
+  const link = 'pear://' + encode(ALIASES.pear)
+  const stream = drop(link, opts)
+  stream.on('data', (msg) => {
+    t.alike({ some: 'info', params: { link: 'pear://pear', ...opts } }, msg)
   })
 })
